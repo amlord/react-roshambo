aaf00bfa598d702688152dfb5288e64b9dc1f55a
Andrew <email@andrewlord.co.uk>
2017-12-19 00:40:06 +0000

    create 'components' folder, and move 'Roshambo' component inside

--- src/components/Roshambo.js
+++ src/components/Roshambo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../logo.svg';
 import './Roshambo.css';
 
 class Roshambo extends Component {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Roshambo from './Roshambo';
+import Roshambo from './components/Roshambo';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Roshambo />, document.getElementById('roshambo'));
