e129e003856b572a436c0dbe48c5a59280f658c6
Andrew <email@andrewlord.co.uk>
2017-12-19 00:37:05 +0000

    added 'enzyme' for shallow tests

--- src/Roshambo.test.js
+++ src/Roshambo.test.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import Roshambo from './Roshambo';
+
+const app = shallow(<Roshambo />);
+
+it('renders correctly', () => {
+  expect(app).toMatchSnapshot();
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
